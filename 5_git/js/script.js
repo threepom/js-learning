9e6ff07875b96775f4a0d36ea74f6dd9c5d2e12f
Sergey <sersalleo@gmail.com>
2023-11-19 12:44:54 +0400

    Change js file in 5_git

--- 5_git/js/script.js
+++ 5_git/js/script.js
@@ -1,18 +1,3 @@
 "use strict";
 
-//  +, -, *, /, %
-//  <, >, &&, ||
-
-let incr = 10;
-let decr = 10;
-
-incr++;
-decr--;
-
-console.log(incr);
-console.log(decr);
-
-console.log(5 % 2);
-
-console.log(2 * 4 == '8');
-console.log(2 * 4 === '8');
+alert("Hello from Git!");
