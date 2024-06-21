bc407d20ebc4196a419ef8b363d9299c71f83408
Sergey <sersalleo@gmail.com>
2024-06-21 22:19:34 +0400

    Add classes to food

--- 39_food/js/script.js
+++ 39_food/js/script.js
@@ -153,4 +153,69 @@ window.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', showModalByScroll);
 
+//     Use classes for cards
+
+    class MenuCard {
+        constructor(imgSrc, imgAlt, title, description, price, parentSelector) {
+            this.imgSrc = imgSrc;
+            this.imgAlt = imgAlt;
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.transfer = 27;
+            this.changeToUAH();
+            this.parent = document.querySelector(parentSelector);
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `<div class="menu__item">
+                    <img src=${this.imgSrc} alt=${this.imgAlt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.description}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>`;
+
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        "Меню \"Фитнес\"",
+        "Меню \"Фитнес\" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. " +
+        "Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+        3,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        "Меню \"Премиум\"",
+        "В меню \"Премиум\" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. " +
+        "Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+        10,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        "Меню \"Постное\"",
+        "Меню \"Постное\" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, " +
+        "молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+        11,
+        ".menu .container"
+    ).render();
+
 });
